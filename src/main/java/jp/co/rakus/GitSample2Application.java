c987f2e23df67f0b4f1d2fc9a6ef3d9af5a7ff2f
uemurakento <kento.uemura@rakus.co.jp>
2018-11-19 11:30:16 +0900

    新規プロジェクト作成

---
+++ src/main/java/jp/co/rakus/GitSample2Application.java
@@ -0,0 +1,12 @@
+package jp.co.rakus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSample2Application.class, args);
+	}
+}
---
+++ src/main/java/jp/co/rakus/Test.java
@@ -0,0 +1,13 @@
+package jp.co.rakus;
+
+public class Test {
+	private int num;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
