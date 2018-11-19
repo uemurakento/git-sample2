89a921db40396dbef9de6770647249305b218bba
uemurakento <kento.uemura@rakus.co.jp>
2018-11-19 11:39:38 +0900

    Testクラスの修正

--- src/main/java/jp/co/rakus/Test.java
+++ src/main/java/jp/co/rakus/Test.java
@@ -2,6 +2,15 @@ package jp.co.rakus;
 
 public class Test {
 	private int num;
+	private String comment;
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 
 	public int getNum() {
 		return num;
